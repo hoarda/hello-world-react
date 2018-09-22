ec12606a5fc96c6f846ae17249fe27871ca53b29
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-22 12:15:18 +0200

    component ornegi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
+function HelloEsma() {
+  return <li>Hello Esma</li>;
+}
+
+function HelloHussein() {
+  return <li>Hello Hussein</li>;
+}
+
+function HelloOktay() {
+  return <li>Hello Oktay</li>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
       <ul>
-        <li>Hello Hussein</li>
-        <li>Hello Esma</li>
-        <li>Hello Oktay</li>
+        <HelloEsma />
+        <HelloHussein />
+        <HelloOktay />
       </ul>
     </div>
   );
